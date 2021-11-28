fde78f5b82b719d8e9b50fc4de221cd0bf891c22
shimizu.kenji <shimizu.kenji@noncore.net>
2021-11-28 11:41:34 +0900

    AppBar追加

---
+++ components/atoms/surfaces/AppBar.tsx
@@ -0,0 +1,17 @@
+import MuiAppBar from "@mui/material/AppBar"
+import MuiToolbar from "@mui/material/Toolbar"
+import {ReactNode} from "react"
+
+type AppBarProps = {
+  children: ReactNode
+}
+
+export default function AppBar({children}: AppBarProps) {
+  return (
+    <MuiAppBar position={"static"}>
+      <MuiToolbar>
+        {children}
+      </MuiToolbar>
+    </MuiAppBar>
+  )
+}
