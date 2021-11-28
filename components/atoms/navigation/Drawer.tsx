bbdb77dcba95432afac4baf72ade6d1eda7c638a
shimizu.kenji <shimizu.kenji@noncore.net>
2021-11-28 11:41:04 +0900

    drawer追加

---
+++ components/atoms/navigation/Drawer.tsx
@@ -0,0 +1,23 @@
+import {ReactNode} from "react"
+import MuiDrawer from "@mui/material/Drawer"
+
+type DrawerAnchor = "left" | "right" | "top" | "bottom"
+
+type DrawerProps = {
+  anchor?: DrawerAnchor
+  open: boolean
+  children: ReactNode
+  onClose: () => void
+}
+
+export default function Drawer({anchor = "left", open, children, onClose}: DrawerProps) {
+  return (
+    <MuiDrawer
+      anchor={anchor}
+      open={open}
+      onClose={onClose}
+    >
+      {children}
+    </MuiDrawer>
+  )
+}
