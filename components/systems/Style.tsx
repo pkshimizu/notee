5b753201d44927e0c20a431aa4daecc26feb0911
shimizu.kenji <shimizu.kenji@noncore.net>
2021-12-19 12:27:30 +0900

    カラーパレットを設定

--- components/systems/Style.tsx
+++ components/systems/Style.tsx
@@ -8,7 +8,40 @@ const cache = createCache({
   prepend: true,
 })
 
-const theme = createTheme({})
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#DEDFEF",
+      main: "#A4A9CF",
+      dark: "#7379AE"
+    },
+    secondary: {
+      light: "#EDD0E5",
+      main: "#C97FB4",
+      dark: "#A53F88"
+    },
+    error: {
+      light: "#F6D4D8",
+      main: "#E38692",
+      dark: "#D04255"
+    },
+    warning: {
+      light: "#FEF7D5",
+      main: "#FBE481",
+      dark: "#F8D32F"
+    },
+    info: {
+      light: "#CBE6F3",
+      main: "#6EB7DB",
+      dark: "#208DC3"
+    },
+    success: {
+      light: "#D1F1CC",
+      main: "#81D674",
+      dark: "#3EBA2B"
+    }
+  }
+})
 
 type StyleProps = {
   children: ReactNode
