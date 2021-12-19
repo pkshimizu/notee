17d69fc977b33416e24ca71b292102aeea179c5f
shimizu.kenji <shimizu.kenji@noncore.net>
2021-12-19 12:27:50 +0900

    fix

--- components/systems/Style.tsx
+++ components/systems/Style.tsx
@@ -11,36 +11,36 @@ const cache = createCache({
 const theme = createTheme({
   palette: {
     primary: {
-      light: "#DEDFEF",
-      main: "#A4A9CF",
-      dark: "#7379AE"
+      light: '#DEDFEF',
+      main: '#A4A9CF',
+      dark: '#7379AE',
     },
     secondary: {
-      light: "#EDD0E5",
-      main: "#C97FB4",
-      dark: "#A53F88"
+      light: '#EDD0E5',
+      main: '#C97FB4',
+      dark: '#A53F88',
     },
     error: {
-      light: "#F6D4D8",
-      main: "#E38692",
-      dark: "#D04255"
+      light: '#F6D4D8',
+      main: '#E38692',
+      dark: '#D04255',
     },
     warning: {
-      light: "#FEF7D5",
-      main: "#FBE481",
-      dark: "#F8D32F"
+      light: '#FEF7D5',
+      main: '#FBE481',
+      dark: '#F8D32F',
     },
     info: {
-      light: "#CBE6F3",
-      main: "#6EB7DB",
-      dark: "#208DC3"
+      light: '#CBE6F3',
+      main: '#6EB7DB',
+      dark: '#208DC3',
     },
     success: {
-      light: "#D1F1CC",
-      main: "#81D674",
-      dark: "#3EBA2B"
-    }
-  }
+      light: '#D1F1CC',
+      main: '#81D674',
+      dark: '#3EBA2B',
+    },
+  },
 })
 
 type StyleProps = {
