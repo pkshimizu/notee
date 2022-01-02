a2fe28eafb751acb73499ff7463de392b999c935
shimizu.kenji <shimizu.kenji@noncore.net>
2022-01-02 11:12:41 +0900

    フォルダパネルのデザインを調整

--- components/organisms/FolderTabPanel.tsx
+++ components/organisms/FolderTabPanel.tsx
@@ -6,6 +6,8 @@ import FolderCard from '../molecules/feedback/FolderCard'
 import NoteCard from '../molecules/feedback/NoteCard'
 import Label from '../atoms/display/Label'
 import Margin from '../atoms/layout/Margin'
+import RelativeBox from "../atoms/layout/RelativeBox";
+import AbsoluteBox from "../atoms/layout/AbsoluteBox";
 
 type FolderTabPanelProps = {
   folder: Folder
@@ -14,34 +16,40 @@ type FolderTabPanelProps = {
 export default function FolderTabPanel({ folder }: FolderTabPanelProps) {
   return (
     <TabPanel value={folder.id}>
-      <FlexColumn>
+      <FlexColumn space={0} height={'100%'}>
         <FolderMenu folder={folder} />
-        <Margin left={2} right={2} bottom={4}>
-          {folder.folders.length > 0 && (
-            <>
-              <Margin top={3} bottom={3}>
-                <Label variant={'caption'}>フォルダ</Label>
+        <FlexRow>
+          <RelativeBox width={'100%'} height={'100%'}>
+            <AbsoluteBox top={0} bottom={0} left={0} right={0}>
+              <Margin left={2} right={2} bottom={4}>
+                {folder.folders.length > 0 && (
+                  <>
+                    <Margin top={3} bottom={3}>
+                      <Label variant={'caption'}>フォルダ</Label>
+                    </Margin>
+                    <FlexRow>
+                      {folder.folders.map((folder) => (
+                        <FolderCard folder={folder} key={folder.id} />
+                      ))}
+                    </FlexRow>
+                  </>
+                )}
+                {folder.notes.length > 0 && (
+                  <>
+                    <Margin top={3} bottom={3}>
+                      <Label variant={'caption'}>ノート</Label>
+                    </Margin>
+                    <FlexRow>
+                      {folder.notes.map((note) => (
+                        <NoteCard note={note} key={note.id} />
+                      ))}
+                    </FlexRow>
+                  </>
+                )}
               </Margin>
-              <FlexRow>
-                {folder.folders.map((folder) => (
-                  <FolderCard folder={folder} key={folder.id} />
-                ))}
-              </FlexRow>
-            </>
-          )}
-          {folder.notes.length > 0 && (
-            <>
-              <Margin top={3} bottom={3}>
-                <Label variant={'caption'}>ノート</Label>
-              </Margin>
-              <FlexRow>
-                {folder.notes.map((note) => (
-                  <NoteCard note={note} key={note.id} />
-                ))}
-              </FlexRow>
-            </>
-          )}
-        </Margin>
+            </AbsoluteBox>
+          </RelativeBox>
+        </FlexRow>
       </FlexColumn>
     </TabPanel>
   )
