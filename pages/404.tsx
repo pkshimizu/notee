40e5873e78ab843356d21e358518e4ffea900ec5
shimizu.kenji <shimizu.kenji@noncore.net>
2022-01-31 22:02:06 +0900

    404ページを追加

---
+++ pages/404.tsx
@@ -0,0 +1,19 @@
+import { FlexColumn, FlexRow } from '../components/atoms/layout/Flex'
+import Label from '../components/atoms/display/Label'
+import Link from '../components/atoms/navigation/Link'
+import Image from '../components/atoms/display/Image'
+
+export default function Error404() {
+  return (
+    <FlexColumn align={'center'} height={'100vh'} width={'100%'}>
+      <FlexRow align={'center'}>
+        <Image url={'/logo.svg'} alt={'logo'} width={128} height={170} />
+        <FlexColumn>
+          <Label variant={'title'}>404</Label>
+          <Label variant={'title'}>Not Found Page</Label>
+          <Link href={'/'}>Back to Home</Link>
+        </FlexColumn>
+      </FlexRow>
+    </FlexColumn>
+  )
+}
