5f5492cb6602707f5ff99c19b18a491fa214130a
shimizu.kenji <shimizu.kenji@noncore.net>
2022-02-01 14:51:57 +0900

    sentryの設定を追加

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const moduleExports = {
   reactStrictMode: true,
   env: {
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
@@ -14,4 +15,20 @@ module.exports = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
-}
+};
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
